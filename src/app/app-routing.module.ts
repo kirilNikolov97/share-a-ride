22f77e07084940709e971577d0db774a10b33d95
kirilnikolov <kiril.nikolov.97@gmail.com>
2020-05-18 18:16:46 +0300

    Small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
 import {ProfileComponent} from './dashboard/profile/profile.component';
